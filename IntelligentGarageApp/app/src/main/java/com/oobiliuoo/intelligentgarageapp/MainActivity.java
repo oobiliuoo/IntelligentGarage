0ae186b33ee2881293be0a49165b37aeb9aa2832
oobiliuoo <biliu819@163.com>
2021-07-03 02:11:16 +0800

    dev: add main ui

--- IntelligentGarageApp/app/src/main/java/com/oobiliuoo/intelligentgarageapp/MainActivity.java
+++ IntelligentGarageApp/app/src/main/java/com/oobiliuoo/intelligentgarageapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.oobiliuoo.intelligentgarageapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.GridView;
 
 import com.oobiliuoo.intelligentgarageapp.adapter.MyGridViewAdapter;
@@ -20,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.Snow2));
+
 
         initView();
 
@@ -40,7 +45,11 @@ public class MainActivity extends AppCompatActivity {
         controlCardList.clear();
         ControlCard card1 = new ControlCard("大厅","吊灯","ON",R.drawable.light_on);
         controlCardList.add(card1);
-        ControlCard card2 = new ControlCard("s","吊灯","OFF",R.drawable.light_on);
+        ControlCard card2 = new ControlCard("室外","大灯","OFF",R.drawable.light_off);
         controlCardList.add(card2);
+        ControlCard card3 = new ControlCard("大厅","门","ON",R.drawable.door_open2);
+        controlCardList.add(card3);
+        ControlCard card4 = new ControlCard("车库","门","OFF",R.drawable.door_closed);
+        controlCardList.add(card4);
     }
 }
